eb9449f33dc1c36744ddd25b027f5bea930d4876
Brenner <francisco.brenner@hotmail.com>
2022-06-04 14:51:19 -0300

    First commit

---
+++ web/src/App.jsx
@@ -0,0 +1,28 @@
+function Tweet({name, username, avatar, children}){
+  return(<div className="flex space-x-3 p-4 border-b border-silver">
+  <div>
+    <img src={avatar} />
+  </div>
+
+  <div>
+    <span className="font-bold text-sm">{name}</span>
+    <span className="text-sm text-silver"> @{username}</span>
+
+    <p>{children}</p>
+  </div>
+</div>)
+}
+
+export function App(){
+  return(
+    <>
+      <Tweet name="Elon Musk" username="elonmusk" avatar="./src/assets/avatar.png">
+      Let's make Twitter maximun fun!
+      </ Tweet>
+
+      <Tweet name="Francisco Brenner" username="franciscobrenner" avatar="./src/assets/avatar.png">
+      Let's make Twitter maximun awesome!
+      </ Tweet>
+    </>
+  )
+}
